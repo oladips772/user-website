e476733d2303dd8870525914039cb956324d53f4
oladips <oladips200@gmail.com>
2022-01-25 11:43:47 +0000

    created error handling functions

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 /** @format */
 const INPUT_DIV = document.querySelector(".input_div");
+const loginBtn = document.querySelector(".loginBtn");
+const verifyBtn = document.querySelector(".verifyBtn");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const emailError = document.querySelector(".email-error");
+const passwordError = document.querySelector(".password-error");
 
 function checkInput() {
   INPUT_DIV.classList.add("active");
-  document.querySelector(".loginBtn").classList.add("inactive");
-  document.querySelector(".verifyBtn").classList.add("active");
+  loginBtn.classList.add("inactive");
+  verifyBtn.classList.add("active");
 }
 
 function verifyInput() {
-  document.querySelector("main").classList.add("inactive");
-  document.querySelector(".welcome_screen").classList.add("active");
+  if (
+    emailInput.value.trim().length < 8 ||
+    passwordInput.value.trim().length < 8
+  ) {
+    emailError.classList.add("active");
+    passwordError.classList.add("active");
+  } else {
+    emailError.classList.remove("active");
+    passwordError.classList.remove("active");
+    document.querySelector("main").classList.add("inactive");
+    document.querySelector(".welcome_screen").classList.add("active");
+  }
 }
